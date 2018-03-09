ba6d5bc024d8f8d9f9c54de9546f40c22df00fed
Marcio Luis da Silva <marcio.luis.da.silva@accenture.com>
2018-03-09 19:52:17 -0300

    Testing

--- src/main/java/com/github/br11/web/api/custom/CustomController.java
+++ src/main/java/com/github/br11/web/api/custom/CustomController.java
@@ -3,11 +3,13 @@ package com.github.br11.web.api.custom;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController
 @RequestMapping("/api")
@@ -16,17 +18,17 @@ public class CustomController {
 	private static long ID_FACTORY = 1;
 	private static final Map<Long, CustomBean> data = new HashMap<>();
 
-	@ApiOperation(value = "Saves a new custom bean", consumes = "the new custom bean data", produces = "the persisted custom bean data with an id")
-	@RequestMapping(value = "/custom", method = RequestMethod.POST)
-	public CustomBean insert(CustomBean bean) {
+	@ApiOperation("Saves a new custom bean")
+	@RequestMapping(value = "/custom", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public CustomBean insert(@ApiParam(value = "the bean", required = true) CustomBean bean) {
 		long id = ID_FACTORY++;
 		data.put(id, bean.setId(id));
 		return get(id);
 	}
 
-	@ApiOperation(value = "Retrives a custom bean data", consumes = "the id of the custom bean to retrieve", produces = "the custom bean data with for the id")
-	@RequestMapping(value = "/custom/{id}", method = RequestMethod.GET)
-	public CustomBean get(Long id) {
+	@ApiOperation("Retrives a custom bean data")
+	@RequestMapping(value = "/custom/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public CustomBean get(@ApiParam(value = "the bean", required = true) Long id) {
 		return data.get(id);
 	}
 
